020bd00be918a20f310579b8fc384c53f01fe9bb
Steve Gardner <gardnersj@gmail.com>
2014-12-02 22:14:05 -0800

    Added opacity change, animation on reset

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-var etch = $('#eas-screen'),		
-		width = 64;
+var etch = $('#eas-screen'),
+	board = $('#eas');
+	$box = $('.box');
 
+// Find RGB background
+function findRBG() {
 
- 
+}
+
+// Change background color
+function changeBackground() {
+
+}
+
 // Grid function
 function grid() {
 
@@ -10,22 +19,26 @@ function grid() {
 	etch.html("");
 	
 	// Create grid
-	var width = $('#new-width').val();
-	var boxSize = 480 / width;
+	var width = $('#new-width').val(),
+		boxSize = 480 / width;
 	
 	for (var i = 0; i < width; i += 1) {
 		var row = $('<div class="row" id="' + i + '"></div>').css('height', boxSize + "px");
 		for (var ib = 0; ib < width; ib += 1) {
-			$('<span class="box"></span>').css('width', boxSize + "px").css('height', boxSize + "px").appendTo(row);
+			$('<span class="box"></span>').css({'width': boxSize + "px", 'height': boxSize + "px"}).appendTo(row);
 		}
 		etch.append(row);
 	}
 	
 	// Color box when mouse hovers
-	$('.box').on(' mouseenter', function() {
-		$(this).css('backgroundColor', 'black');
+	$('.box').mouseenter(function() {
+		if (board.hasClass('erase')) {
+			board.removeClass('erase');
+		}
+		curOp = parseFloat($(this).css('opacity'));
+		$(this).css('opacity', curOp + .1);
 	});
-};
+}
 
 // Set new width on click
 $('#set-width').on('click', grid);
@@ -38,7 +51,8 @@ $('#new-width').on('keydown', function(e) {
 
 // Reset grid
 $('#reset').on('click', function() {
-	$('.box').css('backgroundColor', '');
+	$('#eas').addClass('erase');
+	$('.box').animate({opacity: 0}, 1000);
 });
 
-$(document).ready(grid());
+$(grid);
